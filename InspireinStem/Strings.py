a56d5a3f3cfb38649e5a364e7667a0a7ee3c176c
Moses <66326854+Tripple7@users.noreply.github.com>
2022-05-20 11:22:36 +0300

    Add files via upload

---
+++ InspireinStem/Strings.py
@@ -0,0 +1,49 @@
+name = "Moses gitonga"
+
+# Multiple line strings
+msg= """ QWR54SFX6 confirmed 
+you have received ksh1200 from Janet Mueni. 
+safaricom cares for you """
+print(msg)
+
+city ="nairobi"
+#.upper() is used to convert to upper case
+print(city.upper())
+uni = "JKUAT"
+print(uni.lower())
+
+fruit = "apple"
+print(fruit[:2])
+print(fruit[3:])
+print(fruit[:-1])
+print(fruit[-3:])
+
+#strip() removes spaces before words
+
+f_name= " Bob afwata"
+print(f_name.strip())
+
+f_name = "June"
+s_name = "wambui"
+full_name = f_name + s_name
+print(full_name)
+
+city = "Mombasa"
+print(len(city))
+
+
+city = "Pneumonoultramicroscopicsilicovolcanoconiosis"
+print(len(city))
+
+
+# int- > string
+x = 100  # inter
+
+y = 3.14
+
+z= 1
+
+print(str(x))
+print(int(y))
+
+
---
+++ InspireinStem/calc.py
@@ -0,0 +1,8 @@
+f_num =input("enter the f_number: ")
+s_num =input("enter the s_number: ")
+sum_of_nums =int(f_num)+int(s_num)
+print("The sum of the 2 numbers is",sum_of_nums)
+mult_of_nums= int(f_num) * int(s_num)
+print("the multiplication of the 2 numbers is",mult_of_nums)
+
+
